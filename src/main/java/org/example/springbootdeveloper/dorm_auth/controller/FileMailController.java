641f3f455f060c4ab071b634edee7bdc8353ea0a
sumsum <anakzahad@naver.com>
2023-10-31 19:31:24 +0900

    인증 버튼 추가

---
+++ src/main/java/org/example/springbootdeveloper/dorm_auth/controller/FileMailController.java
@@ -0,0 +1,2 @@
+package org.example.springbootdeveloper.dorm_auth.controller;public class FileMailController {
+}
--- src/main/java/org/example/springbootdeveloper/dorm_auth/controller/FileSendController.kt
+++
@@ -1,9 +0,0 @@
-package org.example.springbootdeveloper.dorm_auth.controller
-
-import org.springframework.stereotype.Controller
-
-@Controller
-class FileCheckController {
-
-
-}
