226deb96f0f473ebd1d5a1abd51a790e4b26bf2e
sumsum <anakzahad@naver.com>
2023-11-13 18:15:24 +0900

    Create /list/total api

--- src/main/java/org/example/springbootdeveloper/recommend/service/DetailService.java
+++ src/main/java/org/example/springbootdeveloper/recommend/service/DetailService.java
@@ -8,12 +8,18 @@ import org.example.springbootdeveloper.recommend.dto.AddUserDetailRequest;
 import org.example.springbootdeveloper.recommend.respository.UserDetailRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class DetailService {
     private final UserDetailRepository userDetailRepository;
-    public UserDetail save(AddUserDetailRequest request, String userId){
-        return userDetailRepository.save(request.toEntity(userId));
+    public UserDetail save(AddUserDetailRequest request, String userId, char gender){
+        return userDetailRepository.save(request.toEntity(userId, gender));
+    }
+
+    public List<UserDetail> showTotalList(String userId){
+        return userDetailRepository.findAll();
     }
 
 }
