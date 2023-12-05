a1f5d752354dc46afffdfd78bf7935ab441d130b
sumsum <anakzahad@naver.com>
2023-12-05 13:54:41 +0900

    id -> nickname 로직 변경

--- src/main/java/org/example/springbootdeveloper/animal_type/dto/AnimalListDTO.java
+++ src/main/java/org/example/springbootdeveloper/animal_type/dto/AnimalListDTO.java
@@ -10,13 +10,15 @@ import java.util.List;
 @Getter
 public class AnimalListDTO {
     private String name;
+    private Boolean sensitive;
     private String features;
     private List<String> wellMatchedRoommates;
     private List<String> incompatibleRoommates;
 
     @Builder
-    public AnimalListDTO(String name, String features, List<String> wellMatchedRoommates, List<String> incompatibleRoommates)
+    public AnimalListDTO(String name, Boolean sensitive, String features, List<String> wellMatchedRoommates, List<String> incompatibleRoommates)
     {
+        this.sensitive = sensitive;
         this.name = name;
         this.features = features;
         this.wellMatchedRoommates = wellMatchedRoommates;
--- src/main/java/org/example/springbootdeveloper/user/respository/UserRepository.java
+++ src/main/java/org/example/springbootdeveloper/user/respository/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId); // UserId로 사용자 정보를 가져옴
+
+    Optional<User> findByNickname(String nickname); // UserId로 사용자 정보를 가져옴
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
     boolean existsByEmail(String email);
